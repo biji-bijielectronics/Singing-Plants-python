7e2bec730c605fea3399e0501621aae268bf797b
Rohan Ahmad <rohanoahmad@gmail.com>
2016-09-08 11:50:15 +0800

    changed led and switch pins

--- shutdown.py
+++ shutdown.py
@@ -1,17 +1,24 @@
 import os
 import time
 import RPi.GPIO as GPIO
+import pyttsx
 
 def shutdown(channel):
+    logfile = open('singing_plants.log', 'a')
+    logfile.write(str(time.time()) + " shutting down")
+    logfile.close()
+    engine = pyttsx.init()
+    engine.say('System Offline')
+    engine.runAndWait()
     print 'shutting down'
     os.system('sudo shutdown -h now')
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(5, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-    GPIO.setup(13, GPIO.OUT)
-    GPIO.add_event_detect(5, GPIO.FALLING, callback = shutdown, bouncetime = 2000)
-    GPIO.output(13, GPIO.HIGH)
+    GPIO.setup(6, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.add_event_detect(6, GPIO.FALLING, callback = shutdown, bouncetime = 2000)
+    GPIO.output(12, GPIO.HIGH)
 
 setup()
 while 1:
